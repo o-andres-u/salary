aa68688fb5148a93331672481b27b8b729539c80
Oscar Andrés Úsuga Nanclares <oscarandresu@gmail.com>
2021-04-09 23:09:22 -0500

    Refactoring Controller

--- src/main/java/com/masglobal/business/controller/SalaryController.java
+++ src/main/java/com/masglobal/business/controller/SalaryController.java
@@ -1,6 +1,6 @@
 package com.masglobal.business.controller;
 
-import com.masglobal.business.model.EmployeeDTO;
+import com.masglobal.business.model.Employee;
 import com.masglobal.business.service.SalaryService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,12 +18,12 @@ public class SalaryController {
   }
 
   @GetMapping("/employees")
-  public List<EmployeeDTO> getEmployeesInformation() {
+  public List<Employee> getEmployeesInformation() {
     return salaryService.getEmployeesInformation();
   }
 
   @GetMapping("/employees/{id}")
-  public EmployeeDTO getEmployeeInformationById(@PathVariable Integer id) {
+  public Employee getEmployeeInformationById(@PathVariable Integer id) {
     return salaryService.getEmployeeInformation(id);
   }
 }
