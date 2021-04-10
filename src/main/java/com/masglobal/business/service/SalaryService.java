96151bb2fd49c913216b7a2e82025e35c4f9d4ac
Oscar Andrés Úsuga Nanclares <oscarandresu@gmail.com>
2021-04-09 23:09:52 -0500

    Refactoring service

--- src/main/java/com/masglobal/business/service/SalaryService.java
+++ src/main/java/com/masglobal/business/service/SalaryService.java
@@ -1,19 +1,37 @@
 package com.masglobal.business.service;
 
-import com.masglobal.business.model.EmployeeDTO;
-import org.springframework.context.annotation.Configuration;
+import com.masglobal.business.factory.EmployeeFactory;
+import com.masglobal.business.model.Employee;
+import com.masglobal.business.model.MasGlobalEmployee;
+import com.masglobal.consumer.EmployeesConsumer;
+import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-@Configuration
+@Component
 public class SalaryService {
 
-  public List<EmployeeDTO> getEmployeesInformation() {
+  private final EmployeesConsumer employeesConsumer;
+  private final EmployeeFactory employeeFactory;
+
+  public SalaryService() {
+    this.employeesConsumer = new EmployeesConsumer();
+    this.employeeFactory = new EmployeeFactory();
+  }
+
+  public List<Employee> getEmployeesInformation() {
+    MasGlobalEmployee[] employees = employeesConsumer.getEmployees();
     return Collections.emptyList();
   }
 
-  public EmployeeDTO getEmployeeInformation(Integer id) {
-    return new EmployeeDTO();
+  public Employee getEmployeeInformation(Integer id) {
+    MasGlobalEmployee[] employees = employeesConsumer.getEmployees();
+    return Arrays.stream(employees)
+        .filter(e -> e.getId().equals(id))
+        .findFirst()
+        .map(employeeFactory.getEmployee)
+        .orElseThrow(IllegalArgumentException::new);
   }
 }
