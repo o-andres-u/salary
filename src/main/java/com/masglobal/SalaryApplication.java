521d5eb81c10e7cb2773df088b58c1d5bc3655ad
Oscar Andrés Úsuga Nanclares <oscarandresu@gmail.com>
2021-04-12 18:51:43 -0500

    Adding CORS configuration

--- src/main/java/com/masglobal/SalaryApplication.java
+++ src/main/java/com/masglobal/SalaryApplication.java
@@ -2,11 +2,24 @@ package com.masglobal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class SalaryApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(SalaryApplication.class, args);
+  }
+
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/employees").allowedOriginPatterns("http://localhost:3000");
+      }
+    };
   }
 }
